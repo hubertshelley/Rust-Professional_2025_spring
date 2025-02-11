a3c5ab987bb30ea2608c395976f7869ad876535e
Hubert Shelley <46239302+hubertshelley@users.noreply.github.com>
2025-02-11 20:12:42 +0800

    feat: 中等第5题完成

--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,4 +1,15 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    if threshold <= 1 {
+        return 0;
+    }
+    let mut a = 1;
+    let mut b = 1;
+    let mut sum = a;
+    while b < threshold {
+        if b % 2 != 0 {
+            sum += b;
+        }
+        (a, b) = (b, a + b);
+    }
+    sum
 }
