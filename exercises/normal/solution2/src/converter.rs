4373a5790f8721bcd9159e684d445511efde7017
Hubert Shelley <46239302+hubertshelley@users.noreply.github.com>
2025-02-11 14:37:01 +0800

    feat: 中等第2题完成

--- exercises/normal/solution2/src/converter.rs
+++ exercises/normal/solution2/src/converter.rs
@@ -1,4 +1,21 @@
 pub fn convert_base(num_str: &str, to_base: u32) -> String {
-    // TODO: 这里写逻辑
-    todo!()
+    let num_str: Vec<_> = num_str.split(&['(', ')'][..]).collect();
+    let (radix_str, from_base): (&str, u32) = (num_str[0], num_str[1].parse().unwrap());
+    let mut num = i64::from_str_radix(radix_str, from_base).unwrap();
+    let mut result = String::new();
+    while num != 0 {
+        let cur_num = (num % to_base as i64) as u8;
+        result.insert(
+            0,
+            ({
+                if cur_num > 10 {
+                    b'a' + (cur_num - 10)
+                } else {
+                    cur_num + b'0'
+                }
+            }) as char,
+        );
+        num /= to_base as i64;
+    }
+    result
 }
